59dbcf50aa5a7323a1cddddf4f4a970d392fe893
Alice <ulmoniss@yandex.ru>
2021-05-31 22:59:37 +0300

    HW0 delete constructor&method

--- src/main/java/ru/javawebinar/topjava/model/UserMealWithExcess.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMealWithExcess.java
@@ -18,14 +18,6 @@ public class UserMealWithExcess {
         this.excess = excess;
     }
 
-    public UserMealWithExcess(UserMeal userMeal, boolean excess) {
-        this(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), excess);
-    }
-
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
     @Override
     public String toString() {
         return "UserMealWithExcess{" +
